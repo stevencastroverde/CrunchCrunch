ee2e1754b44a60e1a4ab4b2bfa9e8999530efc23
Steven Castroverde <stevencastroverde@Stevens-MacBook-Air.local>
2016-12-02 14:38:00 -0700

    added listener and get reques

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
 var obj = require('./obj.js');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+
+
+app.listen(3000, function(){
+  console.log("Listening!");
+
+
+});
+
+app.get('/:name', function(req, res){
+ console.log(req.params);
+ if (obj[req.params.name]){
+   res.json(obj[req.params.name])
+ }
+
+
+})
--- server/obj.js
+++ server/obj.js
@@ -1,16 +1,16 @@
 module.exports = [{
-        name: 'Apple',
-        url: 'https://www.youtube.com/embed/3yDBgIECogM',
+        name: 'Apples',
+        url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/3yDBgIECogM" frameborder="0" allowfullscreen></iframe>',
 
     }, {
         name: 'Chips',
-        url: 'https://www.youtube.com/embed/fEN8zKZBxsE'
+        url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/fEN8zKZBxsE" frameborder="0" allowfullscreen></iframe>'
     }, {
         name: "Carrot",
-        url: "https://www.youtube.com/embed/MJ678OlfAv4"
+        url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/MJ678OlfAv4" frameborder="0" allowfullscreen></iframe>'
     }, {
-        name: "Popcorn",
-        url: "https://www.youtube.com/embed/JhuEXbDtpYY"
+        name: 'Popcorn',
+        url: '<iframe width="560" height="315" src="https://www.youtube.com/embed/JhuEXbDtpYY" frameborder="0" allowfullscreen></iframe>'
 
 }
 
